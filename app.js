504334770e82214b4c38c0041010499ed3f126ab
pleasesailas <pleasesailas@gmail.com>
2023-04-10 16:33:23 +0800

    refactore: modify mongoose config

--- app.js
+++ app.js
@@ -4,20 +4,9 @@ const app = express()
 const PORT = 3000
 
 const routes = require('./routes')
-//mongoose
-const mongoose = require('mongoose')
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
+//mongoose
+require('./config/mongoose')
 //view engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -25,7 +14,6 @@ app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
 //routes
 app.use(routes)
-
 //listening
 app.listen(PORT, () => {
   console.log(`Express is listening on http://localhost:${PORT}`)

d7d1ad9184d82676862c2efc810dfa55a1dc6787
pleasesailas <pleasesailas@gmail.com>
2023-04-10 15:48:46 +0800

    feat:add mongoose & user seeder

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+
 const app = express()
 const PORT = 3000
+
+//mongoose
+const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 //view engine
 app.engine('handlebars', exphbs({ defaultLayout:'main'}))
